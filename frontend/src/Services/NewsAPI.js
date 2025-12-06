efa9b2d5352d2b07792afbb53d3d3ed2d0ad4046
Piyush Maurya <mauryapiyush1011@gmail.com>
2025-12-06 12:11:16 +0530

    updated backend api url

--- frontend/src/Services/NewsAPI.js
+++ frontend/src/Services/NewsAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Base URL for your backend (change when deploying)
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://news-aggregator-e99n.onrender.com";
 
 const fetchTopHeadlines = async (category = "general") => {
   try {
